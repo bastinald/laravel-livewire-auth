43d8c5fc44fc5379a346d048925fee8307ff3985
bastinald <bastinald@icloud.com>
2021-05-11 18:01:53 -0400

    move home view

--- stubs/app/Http/Livewire/Home.php
+++ stubs/app/Http/Livewire/Home.php
@@ -16,7 +16,7 @@ class Home extends Component
 
     public function render()
     {
-        return view('livewire.home')
+        return view('home')
             ->layout('layouts.auth');
     }
 }
